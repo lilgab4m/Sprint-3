b78138d425e4e11e0ddea35c0540ece4345db3ee
credendim <130668967+credendim@users.noreply.github.com>
2023-12-08 11:35:33 -0300

    commit

--- Codigos/navbar/navbar.php
+++ Codigos/navbar/navbar.php
@@ -36,7 +36,11 @@
         }
 
         .navbar {
-
+            z-index: 100;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
             padding: 10px;
             display: flex;
             justify-content: space-between;
