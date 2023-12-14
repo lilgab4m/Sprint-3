7f15277d146e9d80519a16b15fcf6e3c71ba754c
credendim <130668967+credendim@users.noreply.github.com>
2023-12-14 09:38:28 -0300

    coomit

---
+++ Codigos/matricula/matricula.php
@@ -0,0 +1,23 @@
+<?php
+include '../navbar/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='matricula.css'>
+    <title>Matricula</title>
+</head>
+
+<body>
+    <div class="banner">
+        <img src="../../img/matricula.png">
+    </div>
+</body>
+
+</html>
+<?php
+include '../footer/footer.php';
+?>
